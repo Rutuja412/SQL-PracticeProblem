040d21d6df84f99784e7fa1b96c84b9a67be50a4
Rutuja412 <rutujakadam4545@gmail.com>
2023-04-15 09:40:21 +0530

    [Rutuja]:Create Table

--- SQL-Assignment.sql
+++ SQL-Assignment.sql
@@ -3,3 +3,11 @@
 create database PracticeProblem
 --use database
 use PracticeProblem
+--Create Customer table with columns Customer Id, Customer Name,Phone, Address.
+Create Table Customer(
+CustomerID int identity(1,1)Primary key,
+CustomerName varchar(100),
+Address varchar (200),
+PhoneNumber varchar (20)
+);
+
