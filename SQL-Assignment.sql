f38a3e664afea8809475a0cdb545299980030a28
Rutuja412 <rutujakadam4545@gmail.com>
2023-04-15 09:46:43 +0530

    [Rutuja]:Adding customer details in customer Table

--- SQL-Assignment.sql
+++ SQL-Assignment.sql
@@ -10,4 +10,11 @@ CustomerName varchar(100),
 Address varchar (200),
 PhoneNumber varchar (20)
 );
-
+--Add few customer details into customer table.
+insert into Customer (CustomerName,Address,PhoneNumber)
+values ('Rutuja','Satara','9011973870'),
+('Raj','Pune','9850897654'),
+('Dev','Mumbai','9850897654'),
+('Riya','Nagpur','985086767'),
+('Sam','Nashik','8850897654');
+select * from Customer
